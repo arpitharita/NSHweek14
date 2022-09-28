7e0a6065a78e48a14560a4ce7b6b89009c5f37fc
arpitharita <arpit177@gmail.com>
2022-09-27 21:32:20 -0400

    create data

---
+++ target/cucumber-report/cucumber.html/report.js
@@ -0,0 +1,473 @@
+$(document).ready(function() {var formatter = new CucumberHTML.DOMFormatter($('.cucumber-report'));formatter.uri("src/test/java/resources/featurefile/nhs.feature");
+formatter.feature({
+  "line": 1,
+  "name": "NHS Functionality",
+  "description": "\r\nAs a user I want to NHS Costs",
+  "id": "nhs-functionality",
+  "keyword": "Feature"
+});
+formatter.before({
+  "duration": 7231855400,
+  "status": "passed"
+});
+formatter.scenario({
+  "line": 5,
+  "name": "NHS wales cost check",
+  "description": "",
+  "id": "nhs-functionality;nhs-wales-cost-check",
+  "type": "scenario",
+  "keyword": "Scenario"
+});
+formatter.step({
+  "line": 7,
+  "name": "I am on Homepage",
+  "keyword": "Given "
+});
+formatter.step({
+  "line": 8,
+  "name": "I click on start button",
+  "keyword": "When "
+});
+formatter.step({
+  "line": 9,
+  "name": "I click on wales",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 10,
+  "name": "I click on next button",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 11,
+  "name": "I click on yes button",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 12,
+  "name": "I click on next button",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 13,
+  "name": "I click on wales",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 14,
+  "name": "I click on next button",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 15,
+  "name": "I click on day",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 16,
+  "name": "I click on month",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 17,
+  "name": "I click on year",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 18,
+  "name": "I click on link",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 19,
+  "name": "I click on next a button",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 20,
+  "name": "I click on yes button",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 21,
+  "name": "I click on next button",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 22,
+  "name": "I click on no button",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 23,
+  "name": "I click on next button",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 24,
+  "name": "I click on no button",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 25,
+  "name": "I click on next button",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 26,
+  "name": "I click on no button",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 27,
+  "name": "I click on next button",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 28,
+  "name": "I click on no button",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 29,
+  "name": "I click on next button",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 30,
+  "name": "I click on no button",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 31,
+  "name": "I click on next button",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 32,
+  "name": "I click on no button",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 33,
+  "name": "I click on next button",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 34,
+  "name": "I click on no button",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 35,
+  "name": "I click on next button",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 36,
+  "name": "I verify costs \"You get free:\"",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "nhssteps.iAmOnHomepage()"
+});
+formatter.result({
+  "duration": 82852200,
+  "status": "passed"
+});
+formatter.match({
+  "location": "nhssteps.iClickOnStartButton()"
+});
+formatter.result({
+  "duration": 3157963100,
+  "status": "passed"
+});
+formatter.match({
+  "location": "nhssteps.iClickOnWales()"
+});
+formatter.result({
+  "duration": 1074439400,
+  "status": "passed"
+});
+formatter.match({
+  "location": "nhssteps.iClickOnNextButton()"
+});
+formatter.result({
+  "duration": 6203030500,
+  "status": "passed"
+});
+formatter.match({
+  "location": "nhssteps.iClickOnYesButton()"
+});
+formatter.result({
+  "duration": 1146004300,
+  "status": "passed"
+});
+formatter.match({
+  "location": "nhssteps.iClickOnNextButton()"
+});
+formatter.result({
+  "duration": 3834067400,
+  "status": "passed"
+});
+formatter.match({
+  "location": "nhssteps.iClickOnWales()"
+});
+formatter.result({
+  "duration": 1132391300,
+  "status": "passed"
+});
+formatter.match({
+  "location": "nhssteps.iClickOnNextButton()"
+});
+formatter.result({
+  "duration": 3334610400,
+  "status": "passed"
+});
+formatter.match({
+  "location": "nhssteps.iClickOnDay()"
+});
+formatter.result({
+  "duration": 1181255000,
+  "status": "passed"
+});
+formatter.match({
+  "location": "nhssteps.iClickOnMonth()"
+});
+formatter.result({
+  "duration": 1153707400,
+  "status": "passed"
+});
+formatter.match({
+  "location": "nhssteps.iClickOnYear()"
+});
+formatter.result({
+  "duration": 1175001400,
+  "status": "passed"
+});
+formatter.match({
+  "location": "nhssteps.iClickOnLink()"
+});
+formatter.result({
+  "duration": 1122760300,
+  "status": "passed"
+});
+formatter.match({
+  "location": "nhssteps.iClickOnNextAButton()"
+});
+formatter.result({
+  "duration": 4134598600,
+  "status": "passed"
+});
+formatter.match({
+  "location": "nhssteps.iClickOnYesButton()"
+});
+formatter.result({
+  "duration": 1134999400,
+  "status": "passed"
+});
+formatter.match({
+  "location": "nhssteps.iClickOnNextButton()"
+});
+formatter.result({
+  "duration": 3902467300,
+  "status": "passed"
+});
+formatter.match({
+  "location": "nhssteps.iClickOnNoButton()"
+});
+formatter.result({
+  "duration": 1160415500,
+  "status": "passed"
+});
+formatter.match({
+  "location": "nhssteps.iClickOnNextButton()"
+});
+formatter.result({
+  "duration": 3578976700,
+  "status": "passed"
+});
+formatter.match({
+  "location": "nhssteps.iClickOnNoButton()"
+});
+formatter.result({
+  "duration": 1117699600,
+  "status": "passed"
+});
+formatter.match({
+  "location": "nhssteps.iClickOnNextButton()"
+});
+formatter.result({
+  "duration": 3845826600,
+  "status": "passed"
+});
+formatter.match({
+  "location": "nhssteps.iClickOnNoButton()"
+});
+formatter.result({
+  "duration": 1113904700,
+  "status": "passed"
+});
+formatter.match({
+  "location": "nhssteps.iClickOnNextButton()"
+});
+formatter.result({
+  "duration": 3543794900,
+  "status": "passed"
+});
+formatter.match({
+  "location": "nhssteps.iClickOnNoButton()"
+});
+formatter.result({
+  "duration": 1117556000,
+  "status": "passed"
+});
+formatter.match({
+  "location": "nhssteps.iClickOnNextButton()"
+});
+formatter.result({
+  "duration": 3874012300,
+  "status": "passed"
+});
+formatter.match({
+  "location": "nhssteps.iClickOnNoButton()"
+});
+formatter.result({
+  "duration": 1130859600,
+  "status": "passed"
+});
+formatter.match({
+  "location": "nhssteps.iClickOnNextButton()"
+});
+formatter.result({
+  "duration": 5047847100,
+  "status": "passed"
+});
+formatter.match({
+  "location": "nhssteps.iClickOnNoButton()"
+});
+formatter.result({
+  "duration": 1106809700,
+  "status": "passed"
+});
+formatter.match({
+  "location": "nhssteps.iClickOnNextButton()"
+});
+formatter.result({
+  "duration": 5240553200,
+  "status": "passed"
+});
+formatter.match({
+  "location": "nhssteps.iClickOnNoButton()"
+});
+formatter.result({
+  "duration": 1114010600,
+  "status": "passed"
+});
+formatter.match({
+  "location": "nhssteps.iClickOnNextButton()"
+});
+formatter.result({
+  "duration": 5100881400,
+  "status": "passed"
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": "You get free:",
+      "offset": 16
+    }
+  ],
+  "location": "nhssteps.iVerifyCosts(String)"
+});
+formatter.result({
+  "duration": 63304900,
+  "error_message": "java.lang.AssertionError: expected [Based on what you\u0027ve told us\nYou get help with NHS costs] but found [You get free:]\r\n\tat org.testng.Assert.fail(Assert.java:96)\r\n\tat org.testng.Assert.failNotEquals(Assert.java:776)\r\n\tat org.testng.Assert.assertEqualsImpl(Assert.java:137)\r\n\tat org.testng.Assert.assertEquals(Assert.java:118)\r\n\tat org.testng.Assert.assertEquals(Assert.java:453)\r\n\tat org.testng.Assert.assertEquals(Assert.java:463)\r\n\tat Com.NHS.steps.nhssteps.iVerifyCosts(nhssteps.java:86)\r\n\tat ✽.Then I verify costs \"You get free:\"(src/test/java/resources/featurefile/nhs.feature:36)\r\n",
+  "status": "failed"
+});
+formatter.after({
+  "duration": 825062600,
+  "status": "passed"
+});
+formatter.before({
+  "duration": 2911518700,
+  "status": "passed"
+});
+formatter.scenario({
+  "line": 38,
+  "name": "NHS northern ireland",
+  "description": "",
+  "id": "nhs-functionality;nhs-northern-ireland",
+  "type": "scenario",
+  "keyword": "Scenario"
+});
+formatter.step({
+  "line": 40,
+  "name": "I am on Homepage",
+  "keyword": "Given "
+});
+formatter.step({
+  "line": 41,
+  "name": "I click on start button",
+  "keyword": "When "
+});
+formatter.step({
+  "line": 42,
+  "name": "I click on northern ireland",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 43,
+  "name": "I click on next button",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 44,
+  "name": "I verify costs page \"You cannot use this service because you live in Northern Ireland\"",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "nhssteps.iAmOnHomepage()"
+});
+formatter.result({
+  "duration": 35500,
+  "status": "passed"
+});
+formatter.match({
+  "location": "nhssteps.iClickOnStartButton()"
+});
+formatter.result({
+  "duration": 32052700,
+  "error_message": "org.openqa.selenium.NoSuchWindowException: no such window: target window already closed\nfrom unknown error: web view not found\n  (Session info: chrome\u003d105.0.5195.127)\nBuild info: version: \u00274.0.0\u0027, revision: \u00273a21814679\u0027\nSystem info: host: \u0027DESKTOP-7KBRRGO\u0027, ip: \u0027192.168.0.115\u0027, os.name: \u0027Windows 10\u0027, os.arch: \u0027amd64\u0027, os.version: \u002710.0\u0027, java.version: \u00271.8.0_121\u0027\nDriver info: org.openqa.selenium.chrome.ChromeDriver\nCommand: [042225ba3b2c1bc061b1aa99ad1d4d5d, findElement {using\u003dxpath, value\u003d//input[@class\u003d\u0027button button-get-started\u0027]}]\nCapabilities {acceptInsecureCerts: false, browserName: chrome, browserVersion: 105.0.5195.127, chrome: {chromedriverVersion: 105.0.5195.52 (412c95e51883..., userDataDir: C:\\Users\\ARPITK~1\\AppData\\L...}, goog:chromeOptions: {debuggerAddress: localhost:56320}, javascriptEnabled: true, networkConnectionEnabled: false, pageLoadStrategy: normal, platform: WINDOWS, platformName: WINDOWS, proxy: Proxy(), se:cdp: ws://localhost:56320/devtoo..., se:cdpVersion: 105.0.5195.127, setWindowRect: true, strictFileInteractability: false, timeouts: {implicit: 0, pageLoad: 300000, script: 30000}, unhandledPromptBehavior: dismiss and notify, webauthn:extension:credBlob: true, webauthn:extension:largeBlob: true, webauthn:virtualAuthenticators: true}\nSession ID: 042225ba3b2c1bc061b1aa99ad1d4d5d\r\n\tat sun.reflect.NativeConstructorAccessorImpl.newInstance0(Native Method)\r\n\tat sun.reflect.NativeConstructorAccessorImpl.newInstance(NativeConstructorAccessorImpl.java:62)\r\n\tat sun.reflect.DelegatingConstructorAccessorImpl.newInstance(DelegatingConstructorAccessorImpl.java:45)\r\n\tat java.lang.reflect.Constructor.newInstance(Constructor.java:423)\r\n\tat org.openqa.selenium.remote.codec.w3c.W3CHttpResponseCodec.createException(W3CHttpResponseCodec.java:200)\r\n\tat org.openqa.selenium.remote.codec.w3c.W3CHttpResponseCodec.decode(W3CHttpResponseCodec.java:133)\r\n\tat org.openqa.selenium.remote.codec.w3c.W3CHttpResponseCodec.decode(W3CHttpResponseCodec.java:53)\r\n\tat org.openqa.selenium.remote.HttpCommandExecutor.execute(HttpCommandExecutor.java:184)\r\n\tat org.openqa.selenium.remote.service.DriverCommandExecutor.invokeExecute(DriverCommandExecutor.java:164)\r\n\tat org.openqa.selenium.remote.service.DriverCommandExecutor.execute(DriverCommandExecutor.java:139)\r\n\tat org.openqa.selenium.remote.RemoteWebDriver.execute(RemoteWebDriver.java:559)\r\n\tat org.openqa.selenium.remote.ElementLocation$ElementFinder$2.findElement(ElementLocation.java:162)\r\n\tat org.openqa.selenium.remote.ElementLocation.findElement(ElementLocation.java:60)\r\n\tat org.openqa.selenium.remote.RemoteWebDriver.findElement(RemoteWebDriver.java:383)\r\n\tat org.openqa.selenium.remote.RemoteWebDriver.findElement(RemoteWebDriver.java:375)\r\n\tat org.openqa.selenium.support.pagefactory.DefaultElementLocator.findElement(DefaultElementLocator.java:70)\r\n\tat org.openqa.selenium.support.pagefactory.internal.LocatingElementHandler.invoke(LocatingElementHandler.java:39)\r\n\tat com.sun.proxy.$Proxy23.click(Unknown Source)\r\n\tat Com.NHS.utilities.Utility.clickOnElement(Utility.java:53)\r\n\tat Com.NHS.pages.HomePage.ClickOnStart(HomePage.java:26)\r\n\tat Com.NHS.steps.nhssteps.iClickOnStartButton(nhssteps.java:19)\r\n\tat ✽.When I click on start button(src/test/java/resources/featurefile/nhs.feature:41)\r\n",
+  "status": "failed"
+});
+formatter.match({
+  "location": "nhssteps.iClickOnNorthernIreland()"
+});
+formatter.result({
+  "status": "skipped"
+});
+formatter.match({
+  "location": "nhssteps.iClickOnNextButton()"
+});
+formatter.result({
+  "status": "skipped"
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": "You cannot use this service because you live in Northern Ireland",
+      "offset": 21
+    }
+  ],
+  "location": "nhssteps.iVerifyCostsPage(String)"
+});
+formatter.result({
+  "status": "skipped"
+});
+formatter.after({
+  "duration": 9299300,
+  "error_message": "org.openqa.selenium.NoSuchWindowException: no such window: target window already closed\nfrom unknown error: web view not found\n  (Session info: chrome\u003d105.0.5195.127)\nBuild info: version: \u00274.0.0\u0027, revision: \u00273a21814679\u0027\nSystem info: host: \u0027DESKTOP-7KBRRGO\u0027, ip: \u0027192.168.0.115\u0027, os.name: \u0027Windows 10\u0027, os.arch: \u0027amd64\u0027, os.version: \u002710.0\u0027, java.version: \u00271.8.0_121\u0027\nDriver info: org.openqa.selenium.chrome.ChromeDriver\nCommand: [042225ba3b2c1bc061b1aa99ad1d4d5d, screenshot {}]\nCapabilities {acceptInsecureCerts: false, browserName: chrome, browserVersion: 105.0.5195.127, chrome: {chromedriverVersion: 105.0.5195.52 (412c95e51883..., userDataDir: C:\\Users\\ARPITK~1\\AppData\\L...}, goog:chromeOptions: {debuggerAddress: localhost:56320}, javascriptEnabled: true, networkConnectionEnabled: false, pageLoadStrategy: normal, platform: WINDOWS, platformName: WINDOWS, proxy: Proxy(), se:cdp: ws://localhost:56320/devtoo..., se:cdpVersion: 105.0.5195.127, setWindowRect: true, strictFileInteractability: false, timeouts: {implicit: 0, pageLoad: 300000, script: 30000}, unhandledPromptBehavior: dismiss and notify, webauthn:extension:credBlob: true, webauthn:extension:largeBlob: true, webauthn:virtualAuthenticators: true}\nSession ID: 042225ba3b2c1bc061b1aa99ad1d4d5d\r\n\tat sun.reflect.NativeConstructorAccessorImpl.newInstance0(Native Method)\r\n\tat sun.reflect.NativeConstructorAccessorImpl.newInstance(NativeConstructorAccessorImpl.java:62)\r\n\tat sun.reflect.DelegatingConstructorAccessorImpl.newInstance(DelegatingConstructorAccessorImpl.java:45)\r\n\tat java.lang.reflect.Constructor.newInstance(Constructor.java:423)\r\n\tat org.openqa.selenium.remote.codec.w3c.W3CHttpResponseCodec.createException(W3CHttpResponseCodec.java:200)\r\n\tat org.openqa.selenium.remote.codec.w3c.W3CHttpResponseCodec.decode(W3CHttpResponseCodec.java:133)\r\n\tat org.openqa.selenium.remote.codec.w3c.W3CHttpResponseCodec.decode(W3CHttpResponseCodec.java:53)\r\n\tat org.openqa.selenium.remote.HttpCommandExecutor.execute(HttpCommandExecutor.java:184)\r\n\tat org.openqa.selenium.remote.service.DriverCommandExecutor.invokeExecute(DriverCommandExecutor.java:164)\r\n\tat org.openqa.selenium.remote.service.DriverCommandExecutor.execute(DriverCommandExecutor.java:139)\r\n\tat org.openqa.selenium.remote.RemoteWebDriver.execute(RemoteWebDriver.java:559)\r\n\tat org.openqa.selenium.remote.RemoteWebDriver.execute(RemoteWebDriver.java:614)\r\n\tat org.openqa.selenium.remote.RemoteWebDriver.execute(RemoteWebDriver.java:618)\r\n\tat org.openqa.selenium.remote.RemoteWebDriver.getScreenshotAs(RemoteWebDriver.java:349)\r\n\tat Com.NHS.utilities.Utility.getScreenshot(Utility.java:335)\r\n\tat Com.NHS.Hooks.tearDown(Hooks.java:26)\r\n\tat sun.reflect.NativeMethodAccessorImpl.invoke0(Native Method)\r\n\tat sun.reflect.NativeMethodAccessorImpl.invoke(NativeMethodAccessorImpl.java:62)\r\n\tat sun.reflect.DelegatingMethodAccessorImpl.invoke(DelegatingMethodAccessorImpl.java:43)\r\n\tat java.lang.reflect.Method.invoke(Method.java:498)\r\n\tat cucumber.runtime.Utils$1.call(Utils.java:40)\r\n\tat cucumber.runtime.Timeout.timeout(Timeout.java:16)\r\n\tat cucumber.runtime.Utils.invoke(Utils.java:34)\r\n\tat cucumber.runtime.java.JavaHookDefinition.execute(JavaHookDefinition.java:60)\r\n\tat cucumber.runtime.Runtime.runHookIfTagsMatch(Runtime.java:224)\r\n\tat cucumber.runtime.Runtime.runHooks(Runtime.java:212)\r\n\tat cucumber.runtime.Runtime.runAfterHooks(Runtime.java:206)\r\n\tat cucumber.runtime.model.CucumberScenario.run(CucumberScenario.java:46)\r\n\tat cucumber.runtime.junit.ExecutionUnitRunner.run(ExecutionUnitRunner.java:102)\r\n\tat cucumber.runtime.junit.FeatureRunner.runChild(FeatureRunner.java:63)\r\n\tat cucumber.runtime.junit.FeatureRunner.runChild(FeatureRunner.java:18)\r\n\tat org.junit.runners.ParentRunner$3.run(ParentRunner.java:238)\r\n\tat org.junit.runners.ParentRunner$1.schedule(ParentRunner.java:63)\r\n\tat org.junit.runners.ParentRunner.runChildren(ParentRunner.java:236)\r\n\tat org.junit.runners.ParentRunner.access$000(ParentRunner.java:53)\r\n\tat org.junit.runners.ParentRunner$2.evaluate(ParentRunner.java:229)\r\n\tat org.junit.runners.ParentRunner.run(ParentRunner.java:309)\r\n\tat cucumber.runtime.junit.FeatureRunner.run(FeatureRunner.java:70)\r\n\tat cucumber.api.junit.Cucumber.runChild(Cucumber.java:95)\r\n\tat cucumber.api.junit.Cucumber.runChild(Cucumber.java:38)\r\n\tat org.junit.runners.ParentRunner$3.run(ParentRunner.java:238)\r\n\tat org.junit.runners.ParentRunner$1.schedule(ParentRunner.java:63)\r\n\tat org.junit.runners.ParentRunner.runChildren(ParentRunner.java:236)\r\n\tat org.junit.runners.ParentRunner.access$000(ParentRunner.java:53)\r\n\tat org.junit.runners.ParentRunner$2.evaluate(ParentRunner.java:229)\r\n\tat org.junit.internal.runners.statements.RunAfters.evaluate(RunAfters.java:27)\r\n\tat org.junit.runners.ParentRunner.run(ParentRunner.java:309)\r\n\tat cucumber.api.junit.Cucumber.run(Cucumber.java:100)\r\n\tat org.junit.runner.JUnitCore.run(JUnitCore.java:160)\r\n\tat com.intellij.junit4.JUnit4IdeaTestRunner.startRunnerWithArgs(JUnit4IdeaTestRunner.java:69)\r\n\tat com.intellij.rt.junit.IdeaTestRunner$Repeater$1.execute(IdeaTestRunner.java:38)\r\n\tat com.intellij.rt.execution.junit.TestsRepeater.repeat(TestsRepeater.java:11)\r\n\tat com.intellij.rt.junit.IdeaTestRunner$Repeater.startRunnerWithArgs(IdeaTestRunner.java:35)\r\n\tat com.intellij.rt.junit.JUnitStarter.prepareStreamsAndStart(JUnitStarter.java:235)\r\n\tat com.intellij.rt.junit.JUnitStarter.main(JUnitStarter.java:54)\r\n",
+  "status": "failed"
+});
+});
